9230ff119f0e0a90f9d50c641e96f8170b7db021
mchedlo14 <golocogo4@gmail.com>
2023-01-20 05:44:16 -0800

    added generate function

--- src/Components/Generator/GeneratorComp.jsx
+++ src/Components/Generator/GeneratorComp.jsx
@@ -5,10 +5,29 @@ import './GeneratorComp.css'
 const GeneratorComp = () => {
   const [inputRangeVal, setInputRangeVal] = useState('0')
   const [characterLength,setCharacterLength] = useState(0)
+  const [Uppercase,setUpperCase] = useState(false)
+  const [LowerCase,setLowerCase] = useState(false);
+  const [includeNumber,setIncludeNumber] = useState(false)
+  const [includeSymbol,setIncludeSymbol] = useState(false)
 
   useEffect(() => {
     setCharacterLength(parseInt(inputRangeVal))
   },[inputRangeVal])
+
+  const geteratePassword = () => {
+    const numbersArray = [0,1,2,3,4,5,6,7,8,9];
+    const symbolsArray = ['!','@','#','$','%','^','&','*','(',')'];
+
+    const characterCodes = Array.from(Array(26).map((e,i) => i +97));
+    const lowerCaseLetters = characterCodes.map(letter => String.fromCharCode(letter));
+
+    const UpperCaseLetters = lowerCaseLetters.map(letter => letter.toUpperCase());
+
+    
+  }
+
+  geteratePassword()
+
 
 
   return (
@@ -29,22 +48,22 @@ const GeneratorComp = () => {
           </div>
 
           <div className='checkbox-wrapper'>
-            <input type="checkbox" /> 
+            <input type="checkbox" onChange={e => setUpperCase(!Uppercase)}/> 
             <label>Include Uppercase Letters</label>
           </div>
 
           <div className='checkbox-wrapper'>
-            <input type="checkbox" /> 
+            <input type="checkbox" onChange={e => setLowerCase(!LowerCase)}/> 
             <label>Include Lowercase Letters</label>
           </div>
 
           <div className='checkbox-wrapper'>
-            <input type="checkbox" /> 
+            <input type="checkbox" onChange={e => setIncludeNumber(!includeNumber)}/> 
             <label>Include Numbers</label>
           </div>
 
           <div className='checkbox-wrapper'>
-            <input type="checkbox" /> 
+            <input type="checkbox" onChange={e => setIncludeSymbol(!includeSymbol)}/> 
             <label>Include Symbols</label>
           </div>
 
